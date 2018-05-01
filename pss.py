9e2c0526d7af7b86415950e8601f145bfbe2666b
melgino <yehb@qq.com>
2018-05-01 16:16:28 +0800

    This is first action to test github in centos7 with python 3.6
    Signed-off-by: melgino <yehb@qq.com>

---
+++ pss.py
@@ -0,0 +1,18 @@
+import sys
+import mainwindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+####################################
+#
+#
+#
+#
+####################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    # you need to invoke your code yourself
+    ui = mainwindow.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
